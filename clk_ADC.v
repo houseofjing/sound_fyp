c28a68e2d862545baf906bd2d8d961a7aba53d0a
houseofjing <houseofjing@hotmail.com>
2015-07-31 13:36:30 +1000

    added clock enable signals

--- clk_ADC.v
+++ clk_ADC.v
@@ -1,33 +1,20 @@
 module clk_ADC(input 		clk_clk, 
-					output reg 	SCLK,  //if 0, posedge side of CE_SCLK. if 1, negedge side of CE_SCLK
-					input			reset_n/*, 
-					output reg	tempclock*/); //sampling speed of ADC		
+					output 		PE_SCLK,  //if 0, posedge side of CE_SCLK. if 1, negedge side of CE_SCLK
+					output 		NE_SCLK,
+					input			reset_n);
 					
-reg  [7:0] CEcount;
-reg [2:0] four_cycles;	
+reg  [7:0] CEcount;	
 							
-wire CE_SCLK = (CEcount==8'd11); //2.083 MHz clock
+assign PE_SCLK = (CEcount == 8'd0); //2.083 MHz clock
+assign NE_SCLK = ~(CEcount == 8'd11);
 //lowest CEcount possible is ==2'd3 which is a clock of (?) 6.25MHz
 always @(posedge clk_clk)
 begin
-	if (~reset_n)
-	begin
-		CEcount <= 8'h00;
-		SCLK<=1'b0;
-		four_cycles <= 8'h00;
-		//tempclock <= 0;
-	end
+	if (~reset_n) CEcount <= 8'h00;
 	else
 	begin
-		if (CE_SCLK) 
-		begin
-			SCLK <= SCLK+1'b1;
-			four_cycles <= four_cycles+1'b1;
-			CEcount <= 8'h00;
-			//if (four_cycles == 3'b110) tempclock <= 1;
-			//else tempclock <= 0;
-		end
-		else CEcount<=CEcount+1'b1;
+		if (CEcount==8'd23) CEcount <= 8'd0;
+		else CEcount <= CEcount+1;
 	end	
 end
 
