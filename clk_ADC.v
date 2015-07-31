70395f443e5a132a00ef67278b7eff14ae641c8d
houseofjing <houseofjing@hotmail.com>
2015-07-31 17:31:03 +1000

    clock enable stuff added
    outputs both tempclock and clock enables

--- clk_ADC.v
+++ clk_ADC.v
@@ -1,12 +1,16 @@
 module clk_ADC(input 		clk_clk, 
 					output reg 	SCLK,  //if 0, posedge side of CE_SCLK. if 1, negedge side of CE_SCLK
-					input			reset_n/*, 
-					output reg	tempclock*/); //sampling speed of ADC		
-					
-reg  [7:0] CEcount;
-reg [2:0] four_cycles;	
-							
+					input			reset_n,
+					output 		PE_SCLK,  //if 0, posedge side of CE_SCLK. if 1, negedge side of CE_SCLK
+					output 		NE_SCLK);
+reg  [7:0] CEcount;	
+reg  [7:0] CEcount2; //proper clock enable signals							
 wire CE_SCLK = (CEcount==8'd11); //2.083 MHz clock
+
+//proper clock enable code
+assign PE_SCLK = (CEcount2 == 8'd23); 
+assign NE_SCLK = ~(CEcount2 == 8'd11);
+
 //lowest CEcount possible is ==2'd3 which is a clock of (?) 6.25MHz
 always @(posedge clk_clk)
 begin
@@ -14,23 +18,29 @@ begin
 	begin
 		CEcount <= 8'h00;
 		SCLK<=1'b0;
-		four_cycles <= 8'h00;
-		//tempclock <= 0;
 	end
 	else
 	begin
 		if (CE_SCLK) 
 		begin
 			SCLK <= SCLK+1'b1;
-			four_cycles <= four_cycles+1'b1;
 			CEcount <= 8'h00;
-			//if (four_cycles == 3'b110) tempclock <= 1;
-			//else tempclock <= 0;
 		end
-		else CEcount<=CEcount+1'b1;
+		else begin
+			CEcount<=CEcount+1'b1;
+		end
 	end	
 end
 
+always @(posedge clk_clk)
+begin
+	if (~reset_n) CEcount2 <= 8'h00;
+	else
+	begin
+		if (CEcount2==8'd23) CEcount2 <= 8'd0;
+		else CEcount2 <= CEcount2+1;
+	end	
+end
 
 
 
